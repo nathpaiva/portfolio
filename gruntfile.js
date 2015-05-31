01a710f23f0ec473b990739941e2d1946d802ea3
NathPaiva <nathalia.piccoli@e-smart.com.br>
2015-05-31 15:24:32 -0300

    first commit of new porfolio

---
+++ gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function (grunt) {
+  'use strict';
+  // Configurations
+  grunt.initConfig({
+
+    jshint: {
+      files : 'js/*.js'
+    },
+    compass: {
+      dist: {
+        options: {
+          config: 'config.rb',
+          trace: true
+        }
+      }
+    },
+    copy: {
+      main: {
+        // src: ['css/*.css' , 'css/**/*.css']
+        // dest: 'V:/site/uolhost/css/',
+      }
+    },
+    watch: {
+      css: {
+        files: ['SCSS/*.scss', 'SCSS/**/*.scss'],
+        tasks: ['compass']
+      }
+    }
+  });
+
+  // load tasks
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // verificar os JSs
+  grunt.registerTask('hint', ['jshint']);
+  // delivery QA
+  grunt.registerTask('bkpSTG', ['copy:mainSTG']);
+  // grunt.registerTask('bkp', ['copy:bkpQa']);
+  grunt.registerTask('delivery', ['copy:delivery']);
+
+
+  grunt.registerTask('w', ['watch']);
+};
